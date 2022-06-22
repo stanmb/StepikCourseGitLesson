7f5cc33afbad8f13510d574414725b14cf707766
Andrey Yankov <ayankov@ozon.ru>
2022-06-22 18:13:32 +0300

    add click button action to register.py

--- register.py
+++ register.py
@@ -14,6 +14,20 @@ def test_register():
         browser.find_element(By.CSS_SELECTOR, "div.first_block .form-control.second").send_keys("123")
         browser.find_element(By.CSS_SELECTOR, "div.first_block .form-control.third").send_keys("123")
 
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
     finally:
         # ожидание чтобы визуально оценить результаты прохождения скрипта
         time.sleep(10)
