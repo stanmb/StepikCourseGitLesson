651b6eb08f4ca1dadfa97eda049c5328920c0cc9
Andrey Yankov <ayankov@ozon.ru>
2022-06-22 18:12:50 +0300

    add locators to register.py

--- register.py
+++ register.py
@@ -4,3 +4,18 @@ import time
 
 
 
+def test_register():
+    try:
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        browser.find_element(By.CSS_SELECTOR, "div.first_block .form-control.first").send_keys("123")
+        browser.find_element(By.CSS_SELECTOR, "div.first_block .form-control.second").send_keys("123")
+        browser.find_element(By.CSS_SELECTOR, "div.first_block .form-control.third").send_keys("123")
+
+    finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
